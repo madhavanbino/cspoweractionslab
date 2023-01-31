268fbde2349a12c26e6684566243561162bfdcb6
bino.madhavan <bino.madhavan@outlook.com>
2023-01-31 13:31:20 -0600

    Changes Committed

--- GitBranch.bat
+++ GitBranch.bat
@@ -1,9 +1,11 @@
 @echo off
-REM This is a single-line comment in bash
 
-set Timestamp=%date:~4,2%%date:~7,2%%date:~10,4%_%time:~0,2%%time:~3,2%%time:~6,2%
-set branchName=CSALMDemonstration_%Timestamp%
-ECHO %branchName%
+set solutionName=CSALMDemo
+
+set timeStamp=%date:~4,2%%date:~7,2%%date:~10,4%_%time:~0,2%%time:~3,2%%time:~6,2%
+set branchName=%solutionName%_%Timestamp%
+echo %branchName%
+
 
 git switch -c %branchName%
 git add . && git commit -m "Changes Committed"

1bd819e9159fff848ea97d52a02deb80d89b9031
bino.madhavan <bino.madhavan@outlook.com>
2023-01-31 13:49:39 -0600

    Changes Committed

--- GitBranch.bat
+++ GitBranch.bat
@@ -1,5 +1,6 @@
 @echo off
 
+echo ">>Creating local branch and pushing it to Remote>>"
 set solutionName=CSALMDemo
 
 set timeStamp=%date:~4,2%%date:~7,2%%date:~10,4%_%time:~0,2%%time:~3,2%%time:~6,2%
@@ -10,5 +11,7 @@ echo %branchName%
 git switch -c %branchName%
 git add . && git commit -m "Changes Committed"
 git push --set-upstream origin %branchName%
+
+echo Branch created succesfully
 
 
